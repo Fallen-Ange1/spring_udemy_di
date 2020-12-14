35b350668b81eed7596309fb04ea431170ee0935
Pasha <fshrf158@gmail.com>
2020-12-14 15:35:42 +0200

    Modified properties file

--- src/main/java/udemy/springframework/spring_udemy_di/config/PropertyConfig.java
+++ src/main/java/udemy/springframework/spring_udemy_di/config/PropertyConfig.java
@@ -3,12 +3,9 @@ package udemy.springframework.spring_udemy_di.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import udemy.springframework.spring_udemy_di.exampleBeans.FakeDataSource;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
     @Value("${guru.username}")
     String user;
@@ -24,11 +21,5 @@ public class PropertyConfig {
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
-    }
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-
     }
 }
